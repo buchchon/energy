59a447eb6b546dd3310e74bd789aa130cb201d53
buchchon <buchchon@gmail.com>
2026-01-05 16:33:11 +0700

    Create dev-mode.vue

---
+++ dev-mode.vue
@@ -0,0 +1,31 @@
+<template>
+   <router-link to="/db">
+     <button>DASHBOARD</button>
+   </router-link>
+</template>
+
+<style scoped>
+   button {
+       width: 200px;
+       background-color: lightcoral;
+       padding: 10px 30px;
+       text-align: center;
+       color: black;
+       font-family: Arial, Helvetica, sans-serif;
+       border-radius: 5px;
+       border-color: white;
+       position: relative;
+       display: block;
+       margin: auto;
+   }
+
+   button:active {
+       background-color: red;
+       transform: translateY(4px);
+   }
+
+</style>
+
+<script>
+  
+</script>
