b1a7d7da1f0b1d7466b39081e390d2b367fc2216
buchchon <buchchon@gmail.com>
2026-01-05 16:36:01 +0700

    Create homeview.vue

---
+++ homeview.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Header from '@/components/Header.vue';
+import WeeklyReport from '@/components/Weekly-Report.vue'
+import Dashboard from '@/components/Dashboard.vue'
+import Devmode from '@/components/Dev-Mode.vue'
+</script>
+
+<template>
+   <Header />
+ <div class="containter">
+   <WeeklyReport></WeeklyReport>
+ </div>
+  <div class="containter margin">
+   <Dashboard></Dashboard>
+ </div>
+
+ <div class="containter">
+   <Devmode></Devmode>
+ </div>
+</template>
+
+<style scoped>
+</style>
