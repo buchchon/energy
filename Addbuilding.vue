d2053513d73244c64573e6d01e0a2877bc7a2559
buchchon <buchchon@gmail.com>
2026-01-05 16:33:48 +0700

    Create Addbuilding.vue

---
+++ Addbuilding.vue
@@ -0,0 +1,30 @@
+<script>
+
+</script>
+
+<template>
+   <router-link to="/wr">
+     <button>WEEKLY REPORT</button>
+   </router-link>
+</template>
+
+<style scoped>
+   button {
+       width: 200px;
+       background-color: lightblue;
+       padding: 10px 30px;
+       text-align: center;
+       color: black;
+       font-family: Arial, Helvetica, sans-serif;
+       border-radius: 5px;
+       border-color: white;
+       position: relative;
+       display: block;
+       margin: auto;
+   }
+
+   button:active {
+       background-color: red;
+       transform: translateY(4px);
+   }
+</style>
